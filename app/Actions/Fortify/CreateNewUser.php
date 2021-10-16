469a58453667cdd831a664ba2d2f3a5d510d5eeb
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-10-17 02:03:33 +0700

    nd

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -38,6 +38,10 @@ class CreateNewUser implements CreatesNewUsers
             'password' => Hash::make($input['password']),
         ]);
     }
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
     
     
 }
