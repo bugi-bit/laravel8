4e6199fb7511578c5216597f68548d488ff3234e
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-10-19 01:14:11 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
+    public function setPasswordAttribute($password)
+    {
+        $this->fillable['password'] = \Hash::make($password);
+    }
     
 }
