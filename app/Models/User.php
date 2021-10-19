23d61435d85ce732807b0bd37b2b7ae9314d3339
bugi-bit <85178323+bugi-bit@users.noreply.github.com>
2021-10-19 10:07:06 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
     ];
  
     public function setPasswordAttribute($password){
-        $this->attributes['password'] = bcrypt($password);
+         $this->attributes['password'] = \Hash::make($password);
     }
 }
 
